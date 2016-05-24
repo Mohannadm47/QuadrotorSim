c1bcd704e2c508cdaca94cb9ca7dde5752892ef1
Chittaranjan Swaminathan Srinivas <chittaranjan.swaminathan@oru.se>
2016-05-24 17:23:24 +0200

    Height control works with inverse dynamics

--- control.m
+++ control.m
@@ -2,7 +2,8 @@
 
 clear all;
 quadrotor = QuadRotor;
-quadrotor.x(7:9) = [0.1; 0.1; 0.2];
+quadrotor.x(1:3) = [0.0; 0.0; 10.0];
+quadrotor.x(7:9) = [0.0; 0.0; 0.0];
 
 % Simulation times, in seconds.
 start_time = 0;
@@ -17,8 +18,8 @@ N = numel(times);
 x = quadrotor.x;
 
 % Generate a trajectory.
-r_i = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0];
-r_f = [0.0; 0.0; 10.0; 0.0; 0.0; 0.0];
+r_i = [quadrotor.x(1:3); quadrotor.x(7:9)];
+r_f = [1.0; 0.0; 15.0; 0.0; 0.0; 0.0];
 v_i = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0];
 v_f = [0.0; 0.0; 0.0; 0.0; 0.0; 0.0];
 
@@ -56,17 +57,16 @@ for t = times
 
   S = [0;0;0; cross(quadrotor.x(10:12), I*quadrotor.x(10:12))];
 	
-	current_p = quadrotor.x(1:3);
+	current_p = quadrotor.x(1:3)
 	current_v = quadrotor.x(4:6);
-	current_theta = quadrotor.x(7:9);
+	current_theta = quadrotor.x(7:9)
 	current_omega = quadrotor.x(10:12);
 	
 	
 	error(1:3, :) = p(index,1:3)' - current_p;
 	error_dot(1:3, :) = v(index,1:3)' - current_v;
 	error(4:6, :) = R_err(rotation(current_theta'), rotation(p(index,4:6)'));
-	error_dot(4:6, :) = v(index,4:6)' - omega;
-	%error = r - quadrotor.x(3,1);
+	error_dot(4:6, :) = v(index,4:6)' - current_omega;
 	
 	error_mag(index) = error'*error;
 	
@@ -78,8 +78,10 @@ for t = times
 	% Need to rotate the three forces to body co-ordinates.
 	F(1:3) = R_body_to_world*F(1:3);
 	
-	inv(T)*F(3:6)
-	quadrotor.u = inv(T)*F(3:6);
+	u = inv(T)*F(3:6);
+	u(u<0) = 0.0
+	
+	quadrotor.u = u;
 	
 	all_x (:,index) = quadrotor.x;
 	
@@ -91,8 +93,10 @@ for t = times
 	index = index + 1;
 end
 
+final_state = all_x(:, end)
+
+
 figure(1);
 plot(all_x(3,:));
 figure(2);
 plot(error_mag);
-all_x(:, end)
